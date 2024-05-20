7e003ea1dc9de7aaa6a7288d97f2cc18d0ae1f3f
Erick Daniel Padilla <erickdelpavo@gmail.com>
2024-05-20 03:40:23 -0600

    Explicit __all__ for module

--- little_duck/__init__.py
+++ little_duck/__init__.py
@@ -1,3 +1,9 @@
 from .analyzer import LittleDuckAnalyzer
 from .lexer import LittleDuckLexer
 from .parser import LittleDuckParser
+
+__all__ = [
+    'LittleDuckAnalyzer',
+    'LittleDuckLexer',
+    'LittleDuckParser',
+]
