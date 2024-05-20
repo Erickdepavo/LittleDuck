52f559744d043ee61849068ff2ea1eeb36051442
Erick Daniel Padilla <erickdelpavo@gmail.com>
2024-05-20 04:06:38 -0600

    Entire compilation goes inside try block

--- run.py
+++ run.py
@@ -6,31 +6,30 @@ from little_duck.errors import SemanticError
 # Test the compiler
 #
 if __name__ == "__main__":
-    # Build the lexer
-    lexer = LittleDuckLexer()
-    file_contents = ""
-
-    with open('code.ld', 'r') as file:
-        file_contents = file.read()
-
-    tokens = lexer.input(file_contents)
-    result = list(map(lambda x: x.type, tokens))
-    print(result)
-    print("File tokenized successfully")
-
-    # Build the parser
-    parser = LittleDuckParser()
-
-    # Test it
-    tree = parser.parse(file_contents, lexer=LittleDuckLexer())
-    print(tree)
-
-    print("File parsed successfully")
-
-    # Analyze
-    analyzer = LittleDuckAnalyzer(debug=True)
-
     try:
+        # Build the lexer
+        lexer = LittleDuckLexer()
+        file_contents = ""
+
+        with open('code.ld', 'r') as file:
+            file_contents = file.read()
+
+        tokens = lexer.input(file_contents)
+        result = list(map(lambda x: x.type, tokens))
+        print(result)
+        print("File tokenized successfully")
+
+        # Build the parser
+        parser = LittleDuckParser()
+
+        # Test it
+        tree = parser.parse(file_contents, lexer=LittleDuckLexer())
+        print(tree)
+
+        print("File parsed successfully")
+
+        # Analyze
+        analyzer = LittleDuckAnalyzer(debug=True)
         quadruples = analyzer.analyze(tree)
 
         number_width = len(str(len(quadruples)))
@@ -40,6 +39,9 @@ if __name__ == "__main__":
             print(f"{i}:{spaces} ({', '.join(list(map(qstr, quadruple)))})")
 
         print("File analyzed successfully")
+
+    except SyntaxError as error:
+        print("SyntaxError:", error)
 
     except SemanticError as error:
         print("SemanticError:", error.message)
